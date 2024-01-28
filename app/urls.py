9166bcd5dec2fc36ea53b6b204d92fa64657deee
nilanjan <nilanjandolai2003@gmail.com>
2024-01-29 01:45:00 +0530

    small commit

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,7 @@ from app import views
 
 urlpatterns = [
     path('',views.index,name='index'),
-    path('all_members',views.all_members,name='all_members'),
-    path('add_members',views.add_members,name='add_members'),
-    path('filter_members',views.filter_members,name='filter_members'),
+    path('all_members/',views.all_members,name='all_members'),
+    path('add_members/',views.add_members,name='add_members'),
+    path('filter_members/',views.filter_members,name='filter_members'),
 ]
